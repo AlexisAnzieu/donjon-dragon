6719ebf15be883f7c1c9840af083150ae388754b
alexis <alexis.anzieu@gmail.com>
2024-11-07 15:09:25 -0500

    better nav

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -7,37 +7,11 @@ import { GiDoubleDragon } from "react-icons/gi";
 const categories = [
   {
     title: "Personnages",
-    items: [
-      { name: "Classes", href: "/characters/classes" },
-      { name: "Races", href: "/characters/races" },
-      { name: "Historique", href: "/characters/backgrounds" },
-      { name: "Dons", href: "/characters/feats" },
-    ],
-  },
-  {
-    title: "Équipement",
-    items: [
-      { name: "Armes", href: "/equipments?weapons" },
-      { name: "Armures", href: "/equipments?armor" },
-      { name: "Objets Magiques", href: "/equipments?magic-items" },
-      { name: "Outils", href: "/equipments?tools" },
-    ],
-  },
-  {
-    title: "Sorts",
-    items: [
-      { name: "Par Niveau", href: "/spells/by-level" },
-      { name: "Par Classe", href: "/spells/by-class" },
-      { name: "Par École", href: "/spells/by-school" },
-    ],
+    items: [{ name: "Répond au quiz", href: "/character-quiz" }],
   },
   {
     title: "Monstres",
-    items: [
-      { name: "Par FP", href: "/monsters?by-cr" },
-      { name: "Par Type", href: "/monsters?by-type" },
-      { name: "Par Environnement", href: "/monsters?by-environment" },
-    ],
+    href: "/monsters",
   },
 ];
 
@@ -62,31 +36,43 @@ export default function DnDNavigation() {
           <div className="hidden md:flex space-x-4">
             {categories.map((category) => (
               <div key={category.title} className="relative group">
-                <button
-                  className="px-3 py-2 text-white hover:bg-red-800 rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
-                  onClick={() => toggleDropdown(category.title)}
-                >
-                  {category.title}
-                </button>
-                <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-red-950 ring-1 ring-black ring-opacity-5 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-10">
-                  <div
-                    className="py-1"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="options-menu"
+                {category.href ? (
+                  <Link
+                    href={category.href}
+                    className="px-3 py-2 text-white hover:bg-red-800 rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
                   >
-                    {category.items.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className="block px-4 py-2 text-sm text-white hover:bg-red-800 hover:text-white transition-colors duration-200"
-                        role="menuitem"
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
+                    {category.title}
+                  </Link>
+                ) : (
+                  <button
+                    className=" text-white hover:bg-red-800 rounded-md transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+                    onClick={() => toggleDropdown(category.title)}
+                  >
+                    {category.title}
+                  </button>
+                )}
+
+                {category.items?.length && (
+                  <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-red-950 ring-1 ring-black ring-opacity-5 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-10">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      {category.items.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className="block px-4 py-2 text-sm text-white hover:bg-red-800 hover:text-white transition-colors duration-200"
+                          role="menuitem"
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
             ))}
           </div>
@@ -124,25 +110,35 @@ export default function DnDNavigation() {
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             {categories.map((category) => (
               <div key={category.title}>
-                <button
-                  className="w-full text-left px-3 py-2 rounded-md text-base font-medium text-white hover:bg-red-800 hover:text-white transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
-                  onClick={() => toggleDropdown(category.title)}
-                >
-                  {category.title}
-                </button>
-                {activeDropdown === category.title && (
-                  <div className="pl-4 py-2">
-                    {category.items.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-red-800 hover:text-white transition-colors duration-200"
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
+                {category.href ? (
+                  <Link
+                    href={category.href}
+                    className="w-full text-left px-3 py-2 rounded-md text-base font-medium text-white hover:bg-red-800 hover:text-white transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+                  >
+                    {category.title}
+                  </Link>
+                ) : (
+                  <button
+                    className="w-full text-left px-3 py-2 rounded-md text-base font-medium text-white hover:bg-red-800 hover:text-white transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+                    onClick={() => toggleDropdown(category.title)}
+                  >
+                    {category.title}
+                  </button>
                 )}
+                {activeDropdown === category.title &&
+                  category.items?.length && (
+                    <div className="pl-4 py-2">
+                      {category.items.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-red-800 hover:text-white transition-colors duration-200"
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
               </div>
             ))}
           </div>
