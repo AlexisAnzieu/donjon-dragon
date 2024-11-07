93e48b557f46bb01a2db4c6db05f51ac21bc6985
alexis <alexis.anzieu@gmail.com>
2024-11-06 19:18:29 -0500

    appropriate

--- app/monsters/page.tsx
+++ app/monsters/page.tsx
@@ -1,10 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getMonsters } from "@/lib/dd5";
 import MonsterFilter from "../components/monsters/MonsterFilter";
 import type { Monster } from "../api/monsters/route";
 
 export default async function Monsters() {
   const monsters: Monster[] = await getMonsters();
+
+  if (!monsters) {
+    return {
+      notFound: true,
+    };
+  }
 
   return (
     <div className="container mx-auto p-4 min-h-screen">
