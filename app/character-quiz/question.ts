f31966795586ae4df18574039a5cc952a217db7d
alexis <alexis.anzieu@gmail.com>
2024-11-07 15:19:03 -0500

    rewrite questions

--- app/character-quiz/question.ts
+++ app/character-quiz/question.ts
@@ -8,138 +8,156 @@ interface Question {
 
 export const questions: Question[] = [
   {
-    question: "Comment préfères-tu passer ton temps libre ?",
+    question: "Comment préfères-tu passer ton week-end ?",
     answers: [
-      { text: "En communion avec la nature", scores: { Elfe: 2, Barde: 1 } },
       {
-        text: "En partageant un bon repas",
-        scores: { Halfelin: 2, Clerc: 1 },
+        text: "Randonnée et photographie nature",
+        scores: { Elfe: 1, Clerc: 1 },
       },
       {
-        text: "En explorant de nouveaux endroits",
-        scores: { Humain: 2, Roublard: 1 },
+        text: "Dans un café à partager des anecdotes",
+        scores: { Halfelin: 1, Barde: 1 },
       },
       {
-        text: "En forgeant ou en travaillant",
-        scores: { Nain: 2, Guerrier: 1 },
+        text: "À explorer des quartiers inconnus",
+        scores: { Humain: 1, Roublard: 1 },
+      },
+      { text: "Dans mon atelier à bricoler", scores: { Nain: 1, Guerrier: 1 } },
+    ],
+  },
+  {
+    question: "Quel serait ton métier de rêve ?",
+    answers: [
+      {
+        text: "Chercheur en développement durable",
+        scores: { Elfe: 1, Magicien: 1 },
+      },
+      { text: "Détective privé", scores: { Halfelin: 1, Roublard: 1 } },
+      {
+        text: "Influenceur sur les réseaux sociaux",
+        scores: { Humain: 1, Barde: 1 },
+      },
+      { text: "Coach sportif", scores: { Nain: 1, Guerrier: 1 } },
+    ],
+  },
+  {
+    question: "Face à un conflit au travail, tu...",
+    answers: [
+      {
+        text: "Analyses calmement la situation",
+        scores: { Elfe: 1, Guerrier: 1 },
+      },
+      {
+        text: "Cherches à médier et apaiser",
+        scores: { Halfelin: 1, Clerc: 1 },
+      },
+      {
+        text: "Proposes des solutions innovantes",
+        scores: { Humain: 1, Magicien: 1 },
+      },
+      { text: "Négocies habilement", scores: { Nain: 1, Roublard: 1 } },
+    ],
+  },
+  {
+    question: "Où préfères-tu vivre ?",
+    answers: [
+      {
+        text: "Dans un éco-village en périphérie",
+        scores: { Elfe: 1, Barde: 1 },
+      },
+      {
+        text: "Dans un loft historique rénové",
+        scores: { Halfelin: 1, Magicien: 1 },
+      },
+      { text: "En plein centre-ville", scores: { Humain: 1, Clerc: 1 } },
+      {
+        text: "Dans une maison que tu as rénovée",
+        scores: { Nain: 1, Guerrier: 1 },
       },
     ],
   },
   {
-    question: "Quelle est ta plus grande qualité ?",
+    question: "Quelle est ton approche des nouvelles technologies ?",
     answers: [
-      { text: "Ma grâce et mon agilité", scores: { Elfe: 2, Roublard: 1 } },
+      { text: "Expert en cybersécurité", scores: { Elfe: 1, Roublard: 1 } },
       {
-        text: "Ma jovialité et ma chance",
-        scores: { Halfelin: 2, Barde: 1 },
-      },
-      { text: "Mon adaptabilité", scores: { Humain: 2, Magicien: 1 } },
-      { text: "Ma force et mon endurance", scores: { Nain: 2, Guerrier: 1 } },
-    ],
-  },
-  {
-    question: "Quel environnement te plaît le plus ?",
-    answers: [
-      { text: "Les forêts enchantées", scores: { Elfe: 2, Magicien: 1 } },
-      {
-        text: "Les villages pittoresques",
-        scores: { Halfelin: 2, Clerc: 1 },
-      },
-      { text: "Les grandes cités", scores: { Humain: 2, Barde: 1 } },
-      { text: "Les montagnes et cavernes", scores: { Nain: 2, Guerrier: 1 } },
-    ],
-  },
-  {
-    question: "Quel est ton passe-temps favori ?",
-    answers: [
-      { text: "La musique et la poésie", scores: { Elfe: 1, Barde: 2 } },
-      {
-        text: "La cuisine et les festins",
-        scores: { Halfelin: 1, Clerc: 2 },
+        text: "Développeur d'applications innovantes",
+        scores: { Halfelin: 1, Magicien: 1 },
       },
       {
-        text: "Les études et la recherche",
-        scores: { Humain: 1, Magicien: 2 },
+        text: "Spécialiste en intelligence artificielle",
+        scores: { Humain: 1, Guerrier: 1 },
       },
       {
-        text: "Le combat et l'entraînement",
-        scores: { Nain: 1, Guerrier: 2 },
+        text: "Consultant en transformation digitale",
+        scores: { Nain: 1, Clerc: 1 },
       },
     ],
   },
   {
-    question: "Comment réagis-tu face au danger ?",
+    question: "Comment gères-tu tes projets d'équipe ?",
     answers: [
-      { text: "Avec agilité et ruse", scores: { Elfe: 2, Roublard: 1 } },
+      { text: "En favorisant la communication", scores: { Elfe: 1, Barde: 1 } },
       {
-        text: "Avec calme et réflexion",
-        scores: { Halfelin: 2, Magicien: 1 },
+        text: "En trouvant des solutions créatives",
+        scores: { Halfelin: 1, Roublard: 1 },
       },
-      { text: "Avec bravoure et force", scores: { Humain: 2, Guerrier: 1 } },
       {
-        text: "Avec détermination et endurance",
-        scores: { Nain: 2, Clerc: 1 },
+        text: "En guidant avec bienveillance",
+        scores: { Humain: 1, Clerc: 1 },
+      },
+      { text: "En optimisant les processus", scores: { Nain: 1, Magicien: 1 } },
+    ],
+  },
+  {
+    question: "Quel est ton objectif professionnel ?",
+    answers: [
+      {
+        text: "Créer une entreprise éco-responsable",
+        scores: { Elfe: 1, Clerc: 1 },
+      },
+      {
+        text: "Devenir un leader inspirant",
+        scores: { Halfelin: 1, Guerrier: 1 },
+      },
+      { text: "Innover dans ton domaine", scores: { Humain: 1, Magicien: 1 } },
+      {
+        text: "Développer un réseau international",
+        scores: { Nain: 1, Roublard: 1 },
       },
     ],
   },
   {
-    question: "Quel est ton objectif de vie ?",
+    question: "Comment passes-tu tes soirées idéales ?",
     answers: [
-      { text: "Protéger la nature", scores: { Elfe: 2, Clerc: 1 } },
+      { text: "Yoga et méditation", scores: { Elfe: 1, Magicien: 1 } },
+      { text: "Karaoké entre amis", scores: { Halfelin: 1, Barde: 1 } },
       {
-        text: "Vivre en paix et en harmonie",
-        scores: { Halfelin: 2, Barde: 1 },
+        text: "Session de sport ou CrossFit",
+        scores: { Humain: 1, Guerrier: 1 },
       },
-      { text: "Explorer et découvrir", scores: { Humain: 2, Roublard: 1 } },
-      { text: "Forger et créer", scores: { Nain: 2, Magicien: 1 } },
+      { text: "Organisation d'événements", scores: { Nain: 1, Barde: 1 } },
     ],
   },
   {
-    question: "Quel type de magie préfères-tu ?",
+    question: "Quelle est ta plus grande qualité professionnelle ?",
     answers: [
-      { text: "La magie de la nature", scores: { Elfe: 2, Magicien: 1 } },
-      {
-        text: "La magie des illusions",
-        scores: { Halfelin: 2, Roublard: 1 },
-      },
-      { text: "La magie des arcanes", scores: { Humain: 2, Magicien: 1 } },
-      { text: "La magie des runes", scores: { Nain: 2, Magicien: 1 } },
+      { text: "L'empathie et l'écoute", scores: { Elfe: 1, Clerc: 1 } },
+      { text: "L'adaptabilité", scores: { Halfelin: 1, Roublard: 1 } },
+      { text: "Le charisme", scores: { Humain: 1, Barde: 1 } },
+      { text: "La détermination", scores: { Nain: 1, Guerrier: 1 } },
     ],
   },
   {
-    question: "Quel est ton style de combat ?",
+    question: "Quel type de projet te passionne ?",
     answers: [
-      { text: "À distance avec un arc", scores: { Elfe: 2, Guerrier: 1 } },
+      { text: "Les projets de recherche", scores: { Elfe: 1, Magicien: 1 } },
       {
-        text: "Avec des sorts et des enchantements",
-        scores: { Halfelin: 2, Magicien: 1 },
+        text: "Les start-ups innovantes",
+        scores: { Halfelin: 1, Roublard: 1 },
       },
-      {
-        text: "Avec une épée et un bouclier",
-        scores: { Humain: 2, Guerrier: 1 },
-      },
-      {
-        text: "Avec une hache ou un marteau",
-        scores: { Nain: 2, Guerrier: 1 },
-      },
-    ],
-  },
-  {
-    question: "Quel est ton trait de caractère dominant ?",
-    answers: [
-      { text: "La sagesse", scores: { Elfe: 2, Clerc: 1 } },
-      { text: "La chance", scores: { Halfelin: 2, Roublard: 1 } },
-      { text: "La détermination", scores: { Humain: 2, Guerrier: 1 } },
-      { text: "La résilience", scores: { Nain: 2, Guerrier: 1 } },
-    ],
-  },
-  {
-    question: "Quel est ton rôle préféré dans un groupe ?",
-    answers: [
-      { text: "Le protecteur", scores: { Elfe: 2, Clerc: 1 } },
-      { text: "Le soutien", scores: { Halfelin: 2, Barde: 1 } },
-      { text: "Le leader", scores: { Humain: 2, Guerrier: 1 } },
-      { text: "Le combattant", scores: { Nain: 2, Guerrier: 1 } },
+      { text: "Les défis sportifs", scores: { Humain: 1, Guerrier: 1 } },
+      { text: "L'accompagnement d'équipes", scores: { Nain: 1, Clerc: 1 } },
     ],
   },
 ];
