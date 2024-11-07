dc81289b4346d95f771b9330762ca28a3cc633f4
alexis <alexis.anzieu@gmail.com>
2024-11-07 15:28:23 -0500

    fix extended name

--- app/character-quiz/races.ts
+++ app/character-quiz/races.ts
@@ -5,7 +5,7 @@ export const races = [
       "Les elfes sont des êtres gracieux et longévifs, souvent considérés comme les gardiens de la nature et de la magie. Ils possèdent une connexion profonde avec les forêts et les créatures qui y vivent, et leur longévité leur permet d'accumuler une grande sagesse au fil des siècles. Les elfes sont connus pour leurs sens aiguisés, leur agilité et leur maîtrise de l'arc et de la lame. Ils vivent souvent dans des communautés cachées au cœur des bois, où ils pratiquent des arts anciens et des rituels mystiques. Leur culture est riche en traditions et en légendes, et ils vénèrent des divinités liées à la nature et aux étoiles.",
     classes: [
       {
-        name: "Barde - Chanteur des Astres",
+        name: "Barde",
         description:
           "Ce barde elfique est un poète céleste, communiant avec les étoiles et les constellations. Ils chantent des chants anciens qui influencent le destin de leurs alliés et ennemis.",
         abilities: [
@@ -15,7 +15,7 @@ export const races = [
         ],
       },
       {
-        name: "Clerc - Gardien de la Sylve",
+        name: "Clerc",
         description:
           "Clerc dédié aux anciens esprits de la forêt, protégeant la nature et les créatures qui y résident.",
         abilities: [
@@ -25,7 +25,7 @@ export const races = [
         ],
       },
       {
-        name: "Magicien - Maître des Illusions",
+        name: "Magicien",
         description:
           "Les elfes illusionnistes maîtrisent l'art de la tromperie magique, rendant leurs illusions presque réelles.",
         abilities: [
@@ -35,13 +35,13 @@ export const races = [
         ],
       },
       {
-        name: "Guerrier - Épéiste de l'Éclat Lunaire",
+        name: "Guerrier",
         description:
           "Guerrier qui s'entraîne sous la lumière de la lune, utilisant des techniques de combat gracieuses et rapides.",
         abilities: ["Danse de Lune", "Lame de Lumière", "Reflet Lunaire"],
       },
       {
-        name: "Roublard - Veilleur des Ombres",
+        name: "Roublard",
         description:
           "Maître de la furtivité, ce roublard se fond dans les ombres et attaque ses ennemis de manière furtive.",
         abilities: [
@@ -58,7 +58,7 @@ export const races = [
       "Les halfelins, également connus sous le nom de hobbits, sont des petites gens agiles et joyeuses, réputées pour leur amour du confort et de la bonne chère. Ils vivent généralement dans des villages pittoresques et paisibles, entourés de champs fertiles et de jardins bien entretenus. Les halfelins sont des êtres discrets et chanceux, souvent capables de se sortir des situations les plus périlleuses grâce à leur ingéniosité et leur bonne fortune. Leur taille réduite et leur agilité naturelle en font des experts en furtivité et en évasion. Malgré leur apparence innocente, les halfelins possèdent un courage indomptable et une loyauté sans faille envers leurs proches.",
     classes: [
       {
-        name: "Barde - Conteur de Village",
+        name: "Barde",
         description:
           "Un barde halfelin qui tisse des histoires captivantes et ensorcelle ses auditeurs.",
         abilities: [
@@ -68,7 +68,7 @@ export const races = [
         ],
       },
       {
-        name: "Clerc - Guide des Champs",
+        name: "Clerc",
         description:
           "Clerc halfelin dévoué aux esprits de la terre et des récoltes, offrant protection et abondance.",
         abilities: [
@@ -78,7 +78,7 @@ export const races = [
         ],
       },
       {
-        name: "Magicien - Trickster des Rues",
+        name: "Magicien",
         description:
           "Magicien de rue, utilisant des tours de passe-passe et des sorts astucieux pour distraire et tromper.",
         abilities: [
@@ -88,7 +88,7 @@ export const races = [
         ],
       },
       {
-        name: "Guerrier - Protecteur des Jardins",
+        name: "Guerrier",
         description:
           "Guerrier halfelin au grand cœur qui protège les siens et se bat pour la paix de son village.",
         abilities: [
@@ -98,7 +98,7 @@ export const races = [
         ],
       },
       {
-        name: "Roublard - Filou du Quartier",
+        name: "Roublard",
         description:
           "Roublard discret qui sait se faufiler partout et connaît les secrets des ruelles.",
         abilities: ["Pas Silencieux", "Dextérité Agile", "Discrétion Absolue"],
@@ -111,13 +111,13 @@ export const races = [
       "Les humains sont la race la plus polyvalente et la plus répandue dans le monde. Leur adaptabilité et leur ambition leur permettent de s'épanouir dans presque tous les environnements et de réaliser de grandes choses. Les humains sont connus pour leur détermination, leur créativité et leur capacité à surmonter les obstacles. Ils forment des sociétés complexes et diversifiées, allant des royaumes prospères aux cités-états indépendantes. Les humains sont également des explorateurs et des aventuriers, toujours en quête de nouvelles connaissances et de nouvelles terres à découvrir. Leur diversité culturelle et leurs talents variés en font des alliés précieux et des adversaires redoutables.",
     classes: [
       {
-        name: "Barde - Troubadour des Royaumes",
+        name: "Barde",
         description:
           "Barde charismatique qui traverse les royaumes en chantant des histoires et en inspirant les foules.",
         abilities: ["Charisme Captivant", "Chanson de Force", "Poème de Paix"],
       },
       {
-        name: "Clerc - Émissaire des Cieux",
+        name: "Clerc",
         description:
           "Clerc humain consacré aux dieux protecteurs, prodiguant des bénédictions divines et des soins miraculeux.",
         abilities: [
@@ -127,7 +127,7 @@ export const races = [
         ],
       },
       {
-        name: "Magicien - Archimage",
+        name: "Magicien",
         description:
           "Maître de plusieurs écoles de magie, dévoué à l'étude des arcanes pour découvrir les secrets de l'univers.",
         abilities: [
@@ -137,7 +137,7 @@ export const races = [
         ],
       },
       {
-        name: "Guerrier - Champion de Bataille",
+        name: "Guerrier",
         description:
           "Guerrier d'exception, réputé pour ses exploits militaires et son courage au combat.",
         abilities: [
@@ -147,7 +147,7 @@ export const races = [
         ],
       },
       {
-        name: "Roublard - Maître Espion",
+        name: "Roublard",
         description:
           "Roublard humain expert en infiltration, manipulation, et acquisition d'informations confidentielles.",
         abilities: [
@@ -164,7 +164,7 @@ export const races = [
       "Les nains sont des êtres robustes et solides, réputés pour leur passion pour l'artisanat et la bataille. Ils vivent principalement dans des forteresses souterraines, creusées dans les montagnes, où ils exploitent les richesses minérales et forgent des armes et des armures de qualité exceptionnelle. Les nains sont des guerriers redoutables, dotés d'une grande résilience et d'une force physique impressionnante. Leur culture est marquée par un profond respect pour les ancêtres et les traditions, et ils vénèrent des divinités liées à la terre et au feu. Les nains sont également des buveurs et des fêtards, appréciant les festins et les récits épiques de leurs exploits passés.",
     classes: [
       {
-        name: "Barde - Chanteur des Cavernes",
+        name: "Barde",
         description:
           "Barde nain qui chante dans les profondeurs, faisant résonner sa voix dans les cavernes et ravivant le courage de ses alliés.",
         abilities: [
@@ -174,7 +174,7 @@ export const races = [
         ],
       },
       {
-        name: "Clerc - Gardien des Ancêtres",
+        name: "Clerc",
         description:
           "Clerc qui invoque les esprits des ancêtres nains pour guider et protéger son clan.",
         abilities: [
@@ -184,7 +184,7 @@ export const races = [
         ],
       },
       {
-        name: "Magicien - Forgeron des Runes",
+        name: "Magicien",
         description:
           "Magicien spécialisé dans les runes magiques, gravant des enchantements dans les armes et armures.",
         abilities: [
@@ -194,7 +194,7 @@ export const races = [
         ],
       },
       {
-        name: "Guerrier - Défenseur des Halls",
+        name: "Guerrier",
         description:
           "Guerrier robuste, formé pour défendre les halls ancestraux de ses ancêtres contre toute menace.",
         abilities: [
@@ -204,7 +204,7 @@ export const races = [
         ],
       },
       {
-        name: "Roublard - Sapeur de Forteresse",
+        name: "Roublard",
         description:
           "Roublard expert en sabotage et en intrusion dans les tunnels et forteresses souterraines.",
         abilities: [
