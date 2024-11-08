0a69a4c1c9d57c0ba3f77ed2a9e422578cbab9bc
alexis <alexis.anzieu@gmail.com>
2024-11-08 18:36:43 -0500

    add suspense

--- app/character-build/page.tsx
+++ app/character-build/page.tsx
@@ -7,7 +7,7 @@ import ClassSelection from "./ClassSelection";
 import { races } from "./races";
 import { useSearchParams, useRouter } from "next/navigation";
 
-export default function CharacterBuild() {
+function CharacterBuildContent() {
   const searchParams = useSearchParams();
   const characterClass = searchParams.get("characterClass");
   const race = searchParams.get("race");
@@ -39,73 +39,79 @@ export default function CharacterBuild() {
   const selectedRaceData = races.find((race) => race.name === selectedRace);
 
   return (
-    <Suspense>
-      <div className="min-h-screen bg-gray-100 text-gray-800 px-8 py-3">
-        <div className="flex flex-col-reverse lg:flex-row">
-          <div className="w-full lg:w-2/3">
-            <RaceSelection
-              races={races}
-              selectedRace={selectedRace}
-              setSelectedRace={handleRaceChange}
+    <div className="min-h-screen bg-gray-100 text-gray-800 px-8 py-3">
+      <div className="flex flex-col-reverse lg:flex-row">
+        <div className="w-full lg:w-2/3">
+          <RaceSelection
+            races={races}
+            selectedRace={selectedRace}
+            setSelectedRace={handleRaceChange}
+          />
+          {selectedRace && (
+            <ClassSelection
+              selectedRaceData={selectedRaceData}
+              selectedClass={selectedClass}
+              setSelectedClass={handleClassChange}
             />
-            {selectedRace && (
-              <ClassSelection
-                selectedRaceData={selectedRaceData}
-                selectedClass={selectedClass}
-                setSelectedClass={handleClassChange}
-              />
-            )}
-          </div>
-          <div className="w-full lg:w-1/3 lg:sticky lg:top-0 h-screen flex flex-col ">
-            {selectedRace && (
-              <div className="mt-6 text-center text-2xl">
-                Tu es un{" "}
-                <span className="text-primary font-extrabold text-3xl">
-                  {selectedRace} {selectedClass}
-                </span>
+          )}
+        </div>
+        <div className="w-full lg:w-1/3 lg:sticky lg:top-0 h-screen flex flex-col ">
+          {selectedRace && (
+            <div className="mt-6 text-center text-2xl">
+              Tu es un{" "}
+              <span className="text-primary font-extrabold text-3xl">
+                {selectedRace} {selectedClass}
+              </span>
+            </div>
+          )}
+          <div className="flex flex-col items-center mt-6">
+            {!selectedClass && !selectedRace && (
+              <div className="flex flex-col justify-center items-center mt-6 text-center">
+                <button
+                  className="bg-primary text-white py-2 text-2xl px-4 rounded shadow-lg transform transition-transform hover:scale-105 bg-red-700"
+                  onClick={() => {
+                    setSelectedRace(null);
+                    setSelectedClass(null);
+                    router.push(`?`);
+                  }}
+                >
+                  ou fait le test ?
+                </button>
               </div>
             )}
-            <div className="flex flex-col items-center mt-6">
-              {!selectedClass && !selectedRace && (
-                <div className="flex flex-col justify-center items-center mt-6 text-center">
-                  <button
-                    className="bg-primary text-white py-2 text-2xl px-4 rounded shadow-lg transform transition-transform hover:scale-105 bg-red-700"
-                    onClick={() => {
-                      setSelectedRace(null);
-                      setSelectedClass(null);
-                      router.push(`?`);
-                    }}
-                  >
-                    ou fait le test ?
-                  </button>
-                </div>
-              )}
-              {selectedRace && (
-                <Image
-                  className="rounded-full border-4 border-red-700 shadow-xl"
-                  height={300}
-                  width={300}
-                  src={`/img/race/${selectedRace}.jpg`}
-                  alt="Character Image"
-                />
-              )}
-              {selectedClass && (
-                <div className="mt-4 text-center">
-                  <h3 className="text-xl font-bold">Characteristics</h3>
-                  <ul className="list-disc list-inside">
-                    <li>Strength: 10</li>
-                    <li>Dexterity: 12</li>
-                    <li>Constitution: 14</li>
-                    <li>Intelligence: 16</li>
-                    <li>Wisdom: 18</li>
-                    <li>Charisma: 20</li>
-                  </ul>
-                </div>
-              )}
-            </div>
+            {selectedRace && (
+              <Image
+                className="rounded-full border-4 border-red-700 shadow-xl"
+                height={300}
+                width={300}
+                src={`/img/race/${selectedRace}.jpg`}
+                alt="Character Image"
+              />
+            )}
+            {selectedClass && (
+              <div className="mt-4 text-center">
+                <h3 className="text-xl font-bold">Characteristics</h3>
+                <ul className="list-disc list-inside">
+                  <li>Strength: 10</li>
+                  <li>Dexterity: 12</li>
+                  <li>Constitution: 14</li>
+                  <li>Intelligence: 16</li>
+                  <li>Wisdom: 18</li>
+                  <li>Charisma: 20</li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+export default function CharacterBuild() {
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <CharacterBuildContent />
     </Suspense>
   );
 }
