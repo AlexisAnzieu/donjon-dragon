9fd922be7953f264751ab0f32bc13a7087f68416
alexis <alexis.anzieu@gmail.com>
2024-11-17 21:51:18 -0500

    add 9 sort level 1

--- app/character/edit/SpellSelection.tsx
+++ app/character/edit/SpellSelection.tsx
@@ -45,38 +45,40 @@ export default function SpellSelection() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl mx-auto mt-4">
-        {availableSpells.map((spell) => (
-          <div
-            key={spell.name}
-            onClick={() => handleSpellToggle(spell)}
-            className={getSpellCardClassName(spell)}
-          >
-            <div className="p-4 flex-grow flex flex-col">
-              <h2 className="text-xl font-bold text-primary mb-2">
-                {spell.name}
-              </h2>
-              <div className="flex-grow">
-                <p className="text-gray-600">{spell.description}</p>
-              </div>
-            </div>
-            <div className="px-4 py-2 bg-gradient-to-r from-gray-50 to-gray-100 mt-auto border-t border-gray-200">
-              <div className="space-y-3">
-                <div className="flex items-center">
-                  <p className="text-sm font-medium text-gray-700">
-                    Durée:{" "}
-                    <span className="text-gray-600">{spell.duration}</span>
-                  </p>
-                </div>
-                <div className="flex items-center">
-                  <p className="text-sm font-medium text-gray-700">
-                    Portée:{" "}
-                    <span className="text-gray-600">{spell.portée}</span>
-                  </p>
+        {availableSpells
+          .filter((spell) => spell.level === 0)
+          .map((spell) => (
+            <div
+              key={spell.name}
+              onClick={() => handleSpellToggle(spell)}
+              className={getSpellCardClassName(spell)}
+            >
+              <div className="p-4 flex-grow flex flex-col">
+                <h2 className="text-xl font-bold text-primary mb-2">
+                  {spell.name}
+                </h2>
+                <div className="flex-grow">
+                  <p className="text-gray-600">{spell.description}</p>
                 </div>
               </div>
+              <div className="px-4 py-2 bg-gradient-to-r from-gray-50 to-gray-100 mt-auto border-t border-gray-200">
+                <div className="space-y-3">
+                  <div className="flex items-center">
+                    <p className="text-sm font-medium text-gray-700">
+                      Durée:{" "}
+                      <span className="text-gray-600">{spell.duration}</span>
+                    </p>
+                  </div>
+                  <div className="flex items-center">
+                    <p className="text-sm font-medium text-gray-700">
+                      Portée:{" "}
+                      <span className="text-gray-600">{spell.portée}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
